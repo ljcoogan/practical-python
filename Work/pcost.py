646b872db28a39ec18b091ae1cda7c85e730d279
Liam Coogan <git@ljcoogan.com>
2025-05-08 11:33:50 +0100

    Ex 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+
+f = open('Work/Data/portfolio.csv')
+next(f)
+
+sum = 0
+for line in f:
+    row = line.split(',')
+    sum += int(row[1]) * float(row[2])
+
+f.close()
+
+print('Total cost', round(sum, 2))
