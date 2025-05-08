17299a2a9eedd930308251e6920d3f99571f3851
Liam Coogan <git@ljcoogan.com>
2025-05-08 11:48:12 +0100

    Ex 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,21 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.31
 
-f = open('Work/Data/portfolio.csv')
-next(f)
+def portfolio_cost(filename):
+    f = open(filename)
+    next(f)
 
-sum = 0
-for line in f:
-    row = line.split(',')
-    sum += int(row[1]) * float(row[2])
+    sum = 0
+    for line in f:
+        row = line.split(',')
+        try:
+            sum += int(row[1]) * float(row[2])
+        except ValueError:
+            print("Error: Line in file is missing necessary fields.")
 
-f.close()
+    f.close()
+    return sum
 
-print('Total cost', round(sum, 2))
+cost = portfolio_cost('Work/Data/missing.csv')
+print('Total cost', round(cost, 2))
